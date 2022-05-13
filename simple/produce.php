198ba45a3d2ad37d0b4ea435114f885aeeba5234
lvzhenchao <1370164516@qq.com>
2022-05-13 16:12:50 +0800

    Update produce.php

--- simple/produce.php
+++ simple/produce.php
@@ -15,13 +15,13 @@ $connection = new AMQPStreamConnection("127.0.0.1", "5672", "lzc", "lzc", $v_hos
 //创建连接
 $channel = $connection->channel();
 
-//声明队列：着重看参数
+//声明创建队列：着重看参数
 $channel->queue_declare($queue_name, false, true, false, false);//第三个参数：队列持久化
 
 //数据
 $data = "this is message ".date("Y-m-d H:i:s");
 
-//创建消息
+//创建消息并标记消息持久化
 $msg = new AMQPMessage($data, ["delivery_mode" => AMQPMessage::DELIVERY_MODE_PERSISTENT]);//消息持久化
 
 //推到消息队列：将消息发送到队列
